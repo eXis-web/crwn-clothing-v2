9fb9dfc8ed9a5b9cbb6a7eee4578617bfd2fabad
Ksenia <varosianarmen92@gmail.com>
2024-03-17 13:40:19 +0100

    lol

--- src/store/cart/cart.types.ts
+++ src/store/cart/cart.types.ts
@@ -1,4 +1,4 @@
-import { CategoryItem } from "store/categories/category.types";
+import { CategoryItem } from "../../store/categories/category.types";
 
 export enum CART_ACTION_TYPES {
     SET_CART_ITEMS = 'cart/SET_CART_ITEMS',
