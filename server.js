56bbe980d957b2df74e21f58ca2b0392dd236853
Ksenia <varosianarmen92@gmail.com>
2024-03-17 21:40:08 +0100

    reworked a bit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const bodyParser = require('body-parser');
 
 // Middleware for allowing CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 });
 
 app.use(bodyParser.json());
